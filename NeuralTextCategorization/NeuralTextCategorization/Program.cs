7da9d3938dcd6ae78ac9a7796522d1fdd36d666c
Brian Eisenberg <xneoszx@gmail.com>
2017-04-11 14:38:44 -0400

    Added xml parser

---
+++ NeuralTextCategorization/NeuralTextCategorization/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralTextCategorization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlParser xmlParser = new XmlParser();
+        }
+    }
+}
