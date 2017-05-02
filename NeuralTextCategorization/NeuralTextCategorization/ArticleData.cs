469d2cd9e83612dbf517310e139df844c60db169
bheisenberg <xneoszx@gmail.com>
2017-05-02 09:59:33 -0400

    Created a method to get all article words separate from the dictionary

--- NeuralTextCategorization/NeuralTextCategorization/ArticleData.cs
+++ NeuralTextCategorization/NeuralTextCategorization/ArticleData.cs
@@ -8,10 +8,12 @@ public class ArticleData
 {
     public List<string> topics { get; set; }
     public List<string> words { get; set; }
+    public List<string> topWords { get; set; }
 
-    public ArticleData (List<string> topics, List<string> words)
+    public ArticleData (List<string> topics, List<string> words, List<string> topWords)
     {
         this.topics = topics;
         this.words = words;
+        this.topWords = topWords;
     }
 }
--- NeuralTextCategorization/NeuralTextCategorization/XmlParser.cs
+++ NeuralTextCategorization/NeuralTextCategorization/XmlParser.cs
@@ -16,11 +16,9 @@ namespace NeuralTextCategorization
         Dictionary<string, int> words;
         public XmlParser()
         {
-            //Parse(inputFile);
             foreach (string file in files)
             {
                 Debug.WriteLine(file);
-
             }
             Parse(files);
         }
@@ -37,44 +35,54 @@ namespace NeuralTextCategorization
 
         private ArticleData GetArticleData(string[] inputFiles)
         {
-            Dictionary<string, int> wordDict = new Dictionary<string, int>();
+            List<string> words = new List<string>();
             List<string> topics = new List<string>();
-            foreach (string input in inputFiles)
+            List<XElement> articleElements = GetArticleElements(inputFiles);
+            foreach (XElement articleElement in articleElements)
             {
-                XDocument xdoc = CreateXDoc(input);
-                topics.AddRange(GetTopic(xdoc));
-                GetWords(xdoc, wordDict);
+                topics.AddRange(GetTopics(articleElement));
+                words.AddRange(GetWords(articleElement));
             }
-            List<string> words = GetTopWords(wordDict);
-            return new ArticleData(words, topics);
+            List<string> topWords = GetTopWords(words);
+            return new ArticleData(words, topics, topWords);
         }
 
-        private List<string> GetTopWords(Dictionary<string, int> wordDict)
+        private List<string> GetTopWords(List<string> words)
         {
+            List<string> topWords = new List<string>();
+            List<string> filteredWords = new List<string> { "the", "and", "if", "then", "a", "be", "to", "of", "in", "that", "have", "i", "it", "for", "not", "on", "with", "he", "as", "you", "do", "at", "this", "but", "by", "is", "from", "reuter", "reuters", "", "-" };
+            Dictionary<string, int> wordDict = new Dictionary<string, int>();
+            foreach (string word in words)
+            if (!filteredWords.Contains(word))
+            {
+                if (!wordDict.Keys.Contains(word))
+                {
+                    wordDict.Add(word, 0);
+                }
+                wordDict[word] += 1;
+            }
             var wordsOrdered = (from entry in wordDict orderby entry.Value descending select entry).ToList();
             for (int i = 0; i < 1000; i++)
             {
                 Debug.WriteLine(string.Format("{0}: {1}: {2}", i, wordsOrdered[i].Key, wordsOrdered[i].Value));
                 words.Add(wordsOrdered[i].Key);
             }
+            return topWords;
         }
 
-        private List<string> GetTopics(XDocument xdoc)
+        private List<string> GetTopics(XElement articleElement)
         {
             List<string> topics = new List<string>();
-            foreach (var parent in xdoc.Elements())
+            var topicData = from element in articleElement.Elements().Elements()
+                            where element.Name == "TOPICS"
+                            select element;
+            if (topicData.Count() > 0)
             {
-                var topicData = from element in xdoc.Elements().Elements()
-                                where element.Name == "TOPICS"
-                                select element;
-                if (topicData.Count() > 0)
+                foreach (string topic in topicData)
                 {
-                    foreach (string topic in topicData)
+                    if (topic != "" && !topics.Contains(topic))
                     {
-                        if (topic != "" && !topics.Contains(topic))
-                        {
-                            topics.Add(topic);
-                        }
+                        topics.Add(topic);
                     }
                 }
             }
@@ -98,6 +106,42 @@ namespace NeuralTextCategorization
             return xdoc;
         }
 
+        private List<XElement> GetArticleElements(string[] inputFiles)
+        {
+            List<XElement> articleElements = new List<XElement>();
+            foreach (string input in inputFiles)
+            {
+                XDocument xdoc = CreateXDoc(input);
+                foreach (var parent in xdoc.Elements())
+                {
+                    articleElements.Add(parent);
+                }
+            }
+            return articleElements;
+        }
+
+        private List<string> GetWords(XElement articleElement)
+        {
+            List<string> words = new List<string>();
+            foreach (var childElement in articleElement.Elements())
+            {
+                var bodyData = from element in childElement.Elements().Elements()
+                           where element.Name == "BODY"
+                           select element;
+                if (bodyData.Count() > 0)
+                {
+                    string body = bodyData.First().Value.ToLower();
+                    List<string> articleWords = body.Split().ToList();
+                    foreach (string word in articleWords)
+                    {
+                        word.Replace("+", "".Replace(".", "").Replace(",", ""));
+                    }
+                    words = articleWords;
+                }
+            }
+            return words;
+        }
+
         private Dictionary<string, int> GetWords(XDocument xdoc, Dictionary<string, int> wordDict)
         {
             List<string> words = new List<string>();
@@ -116,19 +160,13 @@ namespace NeuralTextCategorization
                         foreach (string word in bodySplit)
                         {
                             word.Replace("+", "".Replace(".", "").Replace(",", ""));
-                            if (!filteredWords.Contains(word))
-                            {
-                                if (!wordDict.Keys.Contains(word))
-                                {
-                                    wordDict.Add(word, 0);
-                                }
-                                wordDict[word] += 1;
-                            }
+
                         }
                     }
                 }
             }
             return wordDict;
         }
+
     }
 }
